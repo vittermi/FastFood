9bf1cd799a06a0cd44407f75c4ca4a7d3c7547a2
Vittorio Ermini <vittorio.ermini@alten.it>
2025-07-18 00:20:37 +0200

    Added missing order Enum

--- fastfood-backend/src/utils/enums.js
+++ fastfood-backend/src/utils/enums.js
@@ -3,6 +3,14 @@ const UserTypes = Object.freeze({
     RESTAURATEUR: 'restaurateur',
 });
 
+const OrderStatus = Object.freeze({
+    ORDERED: 'Ordered',
+    PREPARATION: 'In Preparation',
+    DELIVERY: 'In Delivery',
+    DELIVERED: 'Delivered',
+});
+
 module.exports = {
-    UserTypes
+    UserTypes,
+    OrderStatus,
 };
